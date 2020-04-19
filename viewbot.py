a2bfcb8daeebafc452c644dfea9e47a71c672a7f
Mr Elite_x <Thiwangahack@gmail.com>
2020-04-19 17:57:21 +0530

    Update viewbot.py

--- viewbot.py
+++ viewbot.py
@@ -1,6 +1,6 @@
 from multiprocessing import Pool, cpu_count, freeze_support
 from selenium import webdriver
-from urllib.request import Request, urlopen
+from urllib2 import urlopen
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 from time import sleep
